839ed1296a3b6712d4b5f12db31b507319d4eed0
HermannKpadonou <130051239+HermannKpadonou@users.noreply.github.com>
2024-09-24 23:06:50 +0000

    update my code

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("load", function() {
   // Récupération de l'année actuelle
   const currentYear = document.querySelector("#currentYear");
   const today = new Date();
