981024cad0ec65bc92d1caa48ba3fb4850f421d7
HermannKpadonou <130051239+HermannKpadonou@users.noreply.github.com>
2024-10-01 00:26:39 +0000

    added next script

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,4 +3,13 @@ const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
 navLinks.classList.toggle('active');
-})
+menuToggle.classList.toggle('open');
+});
+
+menuToggle.addEventListener('click', () => {
+if (menuToggle.classList.contains('open')) {
+menuToggle.innerHTML = '<div class="bar"></div><div class="bar"></div><div class="bar"></div>';
+} else {
+menuToggle.innerHTML = '<div class="bar"></div><div class="bar"></div><div class="bar"></div>';
+}
+});
