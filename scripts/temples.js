fb74b38acf4f0844e9c284f6693c6c76a04f8271
HermannKpadonou <130051239+HermannKpadonou@users.noreply.github.com>
2025-03-18 18:17:05 +0000

    assignment update

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,12 +2,12 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
-navLinks.classList.toggle('active');
-menuToggle.classList.toggle('open');
+    navLinks.classList.toggle('active');
+    menuToggle.classList.toggle('open');
 
-if (menuToggle.classList.contains('open')) {
-menuToggle.innerHTML = '×'; // Symbole "X" pour fermer
-} else {
-menuToggle.innerHTML = '☰'; // Symbole hamburger pour ouvrir
-}
+    if (menuToggle.classList.contains('open')) {
+        menuToggle.innerHTML = '×';
+    } else {
+        menuToggle.innerHTML = '☰';
+    }
 });
--- week 2/script/bom.js
+++ week 2/script/bom.js
@@ -9,3 +9,12 @@ li.textContent = input.value;
 deleteButton.textContent = '❌';
 li.append(deleteButton);
 list.append(li);
+button.addEventListener('click', function() {});
+if (input.value.trim() !== '') {
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    input.value = '';
+    input.focus();
+}
